420c37dd67cff4baea23ff4c3e287a459baef894
Vaanik Pandit <vaanikpandit@gmail.com>
2025-09-21 00:47:29 +0530

    Add files via upload

---
+++ lib/feature_box.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'pallete.dart';
+
+class FeatureBox extends StatelessWidget {
+  final String headerText;
+  final String descriptionText; // <-- lowercase d
+  final Color color;
+
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.headerText,
+    required this.descriptionText, // <-- lowercase d
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0).copyWith(left: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 3),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Text(
+                descriptionText, // <-- lowercase d
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
