22771786400cd9bf62ca3c507dbfe2906fb9fe64
Vaanik Pandit <vaanikpandit@gmail.com>
2025-09-24 08:16:58 +0530

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,2 +1,3 @@
 const openAIAPIKey =
-    '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';
+    'Paste your Open-AI API Key';
+
